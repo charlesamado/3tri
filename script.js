58da4e8480e9d7bb060b94225c4454f5b7de69f3
charlesamado <141194317+charlesamado@users.noreply.github.com>
2024-11-22 21:28:37 -0300

    ok

---
+++ script.js
@@ -0,0 +1,4 @@
+// Exemplo de script para adicionar interatividade, como filtros ou animações.
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Biblioteca de Mangás carregada!");
+});
